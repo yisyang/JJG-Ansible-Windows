0c73442781b62de7180611e729d4b315de7e91d9
Scott Yang <yisyang@gmail.com>
2015-06-08 01:53:42 -0700

    Update script to do more conditional checks, use easier official pip install script, and to allow accepting ansible tags from vagrant provisioning

--- windows.sh
+++ windows.sh
@@ -11,47 +11,93 @@
 
 ANSIBLE_PLAYBOOK=$1
 PLAYBOOK_DIR=${ANSIBLE_PLAYBOOK%/*}
+if [[ $# -ge 2 ]]; then
+	ANSIBLE_TAGS=$2
+	ANSIBLE_TAGS_CMD=" --tags ${ANSIBLE_TAGS}"
+else
+	ANSIBLE_TAGS_CMD=""
+fi
 
-# Detect package management system.
-YUM=$(which yum 2>/dev/null)
-APT_GET=$(which apt-get 2>/dev/null)
+ROLE_REQUIREMENTS=$(find /vagrant/$PLAYBOOK_DIR -name "requirements.yml" -o -name "requirements.txt")
+
+# If true, will re-run requirements on each provisioning, if false, will only run requirements when installing ansible
+RERUN_REQUIREMENTS=false
 
 # Make sure Ansible playbook exists.
-if [ ! -f "/vagrant/$ANSIBLE_PLAYBOOK" ]; then
-  echo "Cannot find Ansible playbook."
-  exit 1
+if [[ ! -f /vagrant/$ANSIBLE_PLAYBOOK ]]; then
+	echo "Cannot find Ansible playbook."
+	exit 1
 fi
 
-# Install Ansible and its dependencies if it's not installed already.
-if [ ! -f /usr/bin/ansible ]; then
-  echo "Installing Ansible dependencies and Git."
-  if [[ ! -z ${YUM} ]]; then
-    yum install -y git python python-devel
-  elif [[ ! -z ${APT_GET} ]]; then
-    apt-get install -y git python python-dev
-  else
-    echo "Neither yum nor apt-get are available."
-    exit 1;
-  fi
+echo "Windows machine detected, will run Ansible locally on guest machine"
 
-  echo "Installing pip via easy_install."
-  wget https://raw.githubusercontent.com/ActiveState/ez_setup/v0.9/ez_setup.py
-  python ez_setup.py && rm -f ez_setup.py
-  easy_install pip
-  # Make sure setuptools are installed crrectly.
-  pip install setuptools --no-use-wheel --upgrade
+# Detect various dependencies
+echo "Checking dependencies.";
+declare -a DEPENDENCY_LIST=('yum' 'apt-get' 'python' 'pip' 'ansible', 'git');
+declare -A DEPENDENCY_EXISTS
+for i in "${!DEPENDENCY_LIST[@]}"
+	do
+		if [[ ! -z $(which ${DEPENDENCY_LIST[$i]}) ]]; then
+			DEPENDENCY_EXISTS[${DEPENDENCY_LIST[$i]}]=true
+		else
+			DEPENDENCY_EXISTS[${DEPENDENCY_LIST[$i]}]=false
+		fi
+	done
 
-  echo "Installing required python modules."
-  pip install paramiko pyyaml jinja2 markupsafe
-
-  echo "Installing Ansible."
-  pip install ansible
+# Install git if not found, as many ansible roles used will require it
+if [[ "${DEPENDENCY_EXISTS[git]}" != true ]]; then
+	if [[ "${DEPENDENCY_EXISTS[yum]}" == true ]]; then
+		yum install -y git
+	elif [[ "${DEPENDENCY_EXISTS[apt-get]}" == true ]]; then
+		apt-get install -y git
+	else
+		echo "Neither yum nor apt-get are available."
+		exit 1;
+	fi
 fi
 
-# Install requirements.
-echo "Installing Ansible roles from requirements file, if available."
-find "/vagrant/$PLAYBOOK_DIR" \( -name "requirements.yml" -o -name "requirements.txt" \) -exec sudo ansible-galaxy install -r {} \;
+# Install Ansible if not found
+if [[ "${DEPENDENCY_EXISTS[ansible]}" != true ]]; then
+
+	# Install python if not found
+	if [[ "${DEPENDENCY_EXISTS[python]}" != true ]]; then
+		echo "Installing python"
+		if [[ "${DEPENDENCY_EXISTS[yum]}" == true ]]; then
+			yum install -y python python-devel
+		elif [[ "${DEPENDENCY_EXISTS[apt-get]}" == true ]]; then
+			apt-get install -y python python-dev
+		else
+			echo "Neither yum nor apt-get are available."
+			exit 1;
+		fi
+	fi
+
+	# Install pip using instructions from https://pip.pypa.io/en/stable/installing.html#install-pip
+	if [[ "${DEPENDENCY_EXISTS[pip]}" != true ]]; then
+		echo "Installing pip, setuptools, wheel"
+		wget https://bootstrap.pypa.io/get-pip.py
+		sudo python get-pip.py && rm -f get-pip.py
+	fi
+
+	# Ansible install will also install required modules
+	echo "Installing Ansible"
+	sudo pip install ansible
+
+	RUN_REQUIREMENTS=true
+else
+	echo "Ansible is available"
+
+	RUN_REQUIREMENTS=$RERUN_REQUIREMENTS
+fi
+
+# Install Ansible roles from requirements file, if available.
+if [[ -f "$ROLE_REQUIREMENTS" ]] && [[ $RUN_REQUIREMENTS == true ]]; then
+	echo "Installing Ansible requirements"
+	echo "Found Ansible role file at $ROLE_REQUIREMENTS"
+	sudo ansible-galaxy install -r "${ROLE_REQUIREMENTS}"
+fi
 
 # Run the playbook.
 echo "Running Ansible provisioner defined in Vagrantfile."
-ansible-playbook -i 'localhost,' "/vagrant/${ANSIBLE_PLAYBOOK}" --extra-vars "is_windows=true" --connection=local
+echo "ansible-playbook -i 'localhost,' /vagrant/${ANSIBLE_PLAYBOOK}${ANSIBLE_TAGS_CMD} --extra-vars \"is_windows=true\" --connection=local"
+ansible-playbook -i 'localhost,' /vagrant/${ANSIBLE_PLAYBOOK}${ANSIBLE_TAGS_CMD} --extra-vars "is_windows=true" --connection=local
